046205e5139358f1cebb4777b8fbb36e75696f72
mono-glyceride <glyceride.mono@gmail.com>
2023-08-15 14:31:15 +0900

    add reviews#index#new#create#show

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,2 +1,57 @@
 class ReviewsController < ApplicationController
+  before_action :authenticate_user!
+
+	def index
+		@reviews = current_user.reviews
+	end
+
+	def show
+		@review = Review.find(params[:id])
+	end
+
+	def new
+		@gkpt = GkptInformationForm.new(current_user)
+	end
+
+	def edit
+		@review = Review.find(params[:id])
+	end
+
+	def create
+		@gkpt_information_form = GkptInformationForm.new(current_user, gkpt_information_form_params)
+		if @gkpt_information_form.save
+			flash[:success] = "GKPTが作成されました。"
+			redirect_to reviews_path
+		else
+			render 'new'
+		end
+	end
+
+	def update
+		@gkpt = GkptInformationForm.new(review_params)
+		if @gkpt.valid?
+			@gkpt.save
+			flash[:success] = "GKPTが作成されました。"
+			return redirect_to root_path
+		else
+			render 'new'
+		end
+	end
+
+	def destroy
+    @review = Review.find(params[:id])
+		@review.destroy
+    flash[:success] = 'GKPTを削除しました'
+    redirect_to reviews_url
+	end
+
+	private
+
+	def review_params
+		params.require(:review).permit(:topic)
+	end
+
+	def gkpt_information_form_params
+		params.require(:gkpt_information_form).permit(:topic)
+	end
 end
