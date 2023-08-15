9610806c399420d3e7f5dc18fe3c90bba97daec3
mono-glyceride <glyceride.mono@gmail.com>
2023-08-15 10:06:46 +0900

    command rails db:migrate

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_08_12_062844) do
+ActiveRecord::Schema[7.0].define(version: 2023_08_15_005859) do
+  create_table "reviews", charset: "utf8mb4", force: :cascade do |t|
+    t.string "topic"
+    t.bigint "user_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_reviews_on_user_id"
+  end
+
   create_table "users", charset: "utf8mb4", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
@@ -23,4 +31,5 @@ ActiveRecord::Schema[7.0].define(version: 2023_08_12_062844) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "reviews", "users"
 end
