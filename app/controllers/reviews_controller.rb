0d6d82fedc158d7361e2675c3dfce85dc3886ed3
mono-glyceride <glyceride.mono@gmail.com>
2023-08-15 15:10:53 +0900

    remove 編集機能の削除

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -13,26 +13,11 @@ class ReviewsController < ApplicationController
 		@gkpt = GkptInformationForm.new(current_user)
 	end
 
-	def edit
-		@review = Review.find(params[:id])
-	end
-
 	def create
 		@gkpt_information_form = GkptInformationForm.new(current_user, gkpt_information_form_params)
 		if @gkpt_information_form.save
 			flash[:success] = "GKPTが作成されました。"
 			redirect_to reviews_path
-		else
-			render 'new'
-		end
-	end
-
-	def update
-		@gkpt = GkptInformationForm.new(review_params)
-		if @gkpt.valid?
-			@gkpt.save
-			flash[:success] = "GKPTが作成されました。"
-			return redirect_to root_path
 		else
 			render 'new'
 		end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,4 @@
 Rails.application.routes.draw do
   devise_for :users
-  resources :reviews
+  resources :reviews, only:[ :index, :show, :new, :create, :destroy ]
 end
